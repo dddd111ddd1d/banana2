86f8f5b629d31d9c5a9875e795bd31f79062e23d
dddd111ddd1d <125788795+dddd111ddd1d@users.noreply.github.com>
2023-02-19 09:34:13 +0000

    added type

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ class Tree:
   height = 256.2414
   leafs = 6666667
   age = 101.5
-  width = 2.6767
+  width = 5.1
   
-  def __init__(self):
-    print("1015")
+  def __init__(self, type):
+    self.type = type
 
   def get_older(self):
     self.age += 0.25
@@ -17,9 +17,10 @@ class Tree:
     self.height += 1.99
 
 
-my_tree = Tree()
+my_tree = Tree("baobab")
 
 print(my_tree.age)
+print(my_tree.type)
 
 my_tree.get_older()
 print(my_tree.age)
